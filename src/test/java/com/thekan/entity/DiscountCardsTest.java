de9ba9937fc32ccdd63a9a6b934c79dcfb252108
Andrey <57009617+thekanas@users.noreply.github.com>
2022-12-20 15:12:57 +0300

    update class DiscountCardsTest

--- src/test/java/com/thekan/entity/DiscountCardsTest.java
+++ src/test/java/com/thekan/entity/DiscountCardsTest.java
@@ -5,8 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiscountCardsTest {
+    DiscountCard discountCard = new DiscountCard("13a", 16);
+    List<DiscountCard> list = new ArrayList<>();
+    list.add(discountCard);
+    DiscountCards discountCards = new DiscountCards(list);
 
     @Test
     void getDiscountCard() {
+        assertEquals (discountCard, discountCards.getDiscountCard("13a"));
+    }
+    
+    @Test
+    void getDiscountCard() {
+        assertNull (discountCards.getDiscountCard("13"));
     }
 }
