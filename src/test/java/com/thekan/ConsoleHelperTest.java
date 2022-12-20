be605f0b970e7211064993562eebea98dfc8680e
Andrey <57009617+thekanas@users.noreply.github.com>
2022-12-20 15:35:22 +0300

    update class ConsoleHelperTest

--- src/test/java/com/thekan/ConsoleHelperTest.java
+++ src/test/java/com/thekan/ConsoleHelperTest.java
@@ -8,6 +8,13 @@ class ConsoleHelperTest {
 
     @Test
     void print() {
+        private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        private final PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+        System.out.print("hello");
+        System.setOut(originalOut);
+        
+        assertEquals("hello", outContent.toString());
     }
 
     @Test
