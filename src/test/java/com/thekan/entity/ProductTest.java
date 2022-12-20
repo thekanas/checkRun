ec8d2add31f47c71a425f7ea81e5e00e37cd70bc
Andrey <57009617+thekanas@users.noreply.github.com>
2022-12-20 14:27:29 +0300

    update class ProductTest

--- src/test/java/com/thekan/entity/ProductTest.java
+++ src/test/java/com/thekan/entity/ProductTest.java
@@ -9,6 +9,7 @@ class ProductTest {
 
 
     Product product = new Product("1a", "Banana", 1.99, 0.05, true, 1.5, 10);
+    double delta = 0.0000001;
 
     @Test
     void getId() {
@@ -17,25 +18,31 @@ class ProductTest {
 
     @Test
     void getName() {
+        assertEquals ("Banana", product.getName());
     }
 
     @Test
     void getPrice() {
+        assertEquals (1.99, product.getPrice(), delta);
     }
 
     @Test
     void getTax() {
+        assertEquals (0.05, product.getTax(), delta);
     }
 
     @Test
     void isDiscount() {
+        assertEquals (true, product.isDiscount());
     }
 
     @Test
     void getQuantityForDiscount() {
+        assertEquals (1.5, product.getQuantityForDiscount(), delta);
     }
 
     @Test
     void getProcentDiscount() {
+        assertEquals (10, product.getProcentDiscount());
     }
 }
