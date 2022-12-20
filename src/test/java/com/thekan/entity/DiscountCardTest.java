884c8a5aeb7dc285c647704509a3ee0a2d7a3c94
Andrey <57009617+thekanas@users.noreply.github.com>
2022-12-20 15:09:30 +0300

    update class DiscountCardTest

--- src/test/java/com/thekan/entity/DiscountCardTest.java
+++ src/test/java/com/thekan/entity/DiscountCardTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiscountCardTest {
+    DiscountCard discountCard = new DiscountCard("12a", 13);
 
     @Test
     void getId() {
+        assertEquals ("12a", discountCard.getId());
     }
 
     @Test
     void getProcentDiscount() {
+       assertEquals (13, discountCard.getProcentDiscount()); 
     }
 }
