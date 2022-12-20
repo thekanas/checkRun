24498279a26824334cdec7418eb63068b68efa9d
Andrey <57009617+thekanas@users.noreply.github.com>
2022-12-20 15:06:11 +0300

    update class ProductsTest

--- src/test/java/com/thekan/entity/ProductsTest.java
+++ src/test/java/com/thekan/entity/ProductsTest.java
@@ -5,8 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductsTest {
+    Product product = new Product("1a", "Banana", 1.99, 0.05, true, 1.5, 10);
+    List<Product> list = new ArrayList<>();
+    list.add(product);
+    Products products = new Products(list);
+    
 
     @Test
     void getProduct() {
+        assertEquals (product, products.getProduct("1a"));
+    }
+    @Test
+    void getProduct() {
+        assertNull (products.getProduct("2"));
     }
 }
