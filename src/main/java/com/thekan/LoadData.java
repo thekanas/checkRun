2ae0a39e79c24b98031c5b5b29134559a35afef5
Andrey <57009617+thekanas@users.noreply.github.com>
2022-12-20 15:39:56 +0300

    update class LoadData

--- src/main/java/com/thekan/LoadData.java
+++ src/main/java/com/thekan/LoadData.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LoadData {
-    //класс разделяет в входящие данные на
-    //список покупок, дисконтную карту, путь к файлам с товароми и диск.картами
+    //класс разделяет входящие данные на
+    //список покупок, дисконтную карту, пути к файлам с товароми и диск.картами
     private Products products;
     private DiscountCards discountCards;
     private boolean isDiscountCardPresent;
@@ -32,10 +32,10 @@ public class LoadData {
         //card-id - id номер дисконтной карты (не указывается если дисконтная карта не предъяевлена)
         //***.txt - путь к файлу с товарами c расширением .txt (указывается всегда)
         //***.txt - путь к файлу с дисконтными картами c расширением .txt (не указывается если дисконтная карта не предъяевлена)
-        if ((args[args.length-2]).toLowerCase().endsWith("txt")) {
+        if ((args[args.length-2]).toLowerCase().endsWith(".txt")) {
             products = new Products(args[args.length-2]);
 
-            if((args[args.length-3]).toLowerCase().startsWith("card")) {
+            if((args[args.length-3]).toLowerCase().startsWith("card-")) {
                 isDiscountCardPresent = true;
                 discountCardNumber = (args[args.length - 3]).substring(5);
                 discountCards = new DiscountCards(args[args.length-1]);
